7fe731546a7535d9f13d2d9a6b8c37720804cd80
Farouq Atoyebi <farouqatoyebi@gmail.com>
2026-03-03 09:57:49 +0100

    Changed the forgot password to now take mobile and country code to update a password

--- user_api/u_forget_password.php
+++ user_api/u_forget_password.php
@@ -4,27 +4,29 @@ require dirname(dirname(__FILE__)) . '/include/estate.php';
 header('Content-type: text/json');
 $data = json_decode(file_get_contents('php://input'), true);
 
-$email   = $data['email'];
+$ccode   = $data['ccode'];
+$mobile   = $data['mobile'];
 $password = $data['password'];
-if ($email == '' or $password == '') {
-    $returnArr = ["ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "The email, password and code fields are required. Please check and try again!"];
+if ($ccode == '' or $mobile == '' or $password == '') {
+    $returnArr = ["ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "The country code, mobile, password and code fields are required. Please check and try again!"];
 } else {
 
-    $email   = strip_tags(mysqli_real_escape_string($rstate, $email));
+    $ccode   = strip_tags(mysqli_real_escape_string($rstate, $ccode));
+    $mobile   = strip_tags(mysqli_real_escape_string($rstate, $mobile));
     $password = strip_tags(mysqli_real_escape_string($rstate, $password));
 
-    $counter = $rstate->query("SELECT * FROM `tbl_user` WHERE `email` = '" . $email . "'");
+    $counter = $rstate->query("SELECT * FROM `tbl_user` WHERE `mobile` = '{$mobile}' AND `ccode` = '{$ccode}' ORDER BY `id` DESC LIMIT 1");
 
     if ($counter->num_rows != 0) {
         $table = "tbl_user";
         $field = ['password' => $password];
-        $where = "where email='" . $email . "'";
+        $where = "WHERE `mobile` = '{$mobile}' AND `ccode` = '{$ccode}' ORDER BY `id` DESC LIMIT 1";
         $h     = new Estate();
         $check = $h->restateupdateData_Api($field, $table, $where);
 
         $returnArr = ["ResponseCode" => "200", "Result" => "true", "ResponseMsg" => "Password Changed Successfully!!!!!"];
     } else {
-        $returnArr = ["ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "email Not Matched!!!!"];
+        $returnArr = ["ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "mobile Not Matched!!!!"];
     }
 }
 
